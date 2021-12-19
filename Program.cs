fda89a8c1c1235ec4233feb156da97735a929613
afaki16 <alperenfaki@gmail.com>
2021-12-19 22:07:39 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Net_Practice
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhabalar" + "" + name + " " + surname);
+            
+        }
+    }
+}
