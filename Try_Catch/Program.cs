6d9c71e3b70e598e83cad02d727e92e03909bac8
afaki16 <alperenfaki@gmail.com>
2021-12-21 22:47:30 +0300

    Last

---
+++ Try_Catch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
