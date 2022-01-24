5be7e884900f66966be737b24350e9d1cae5230e
afaki16 <alperenfaki@gmail.com>
2022-01-24 21:12:58 +0300

    Last one

--- Try_Catch/Program.cs
+++ Try_Catch/Program.cs
@@ -8,7 +8,64 @@ namespace Try_Catch
        
        static void Main(string[] args)
        {
-         Console.WriteLine("Bir sayı giriniz :");
+         try
+         {
+             Console.WriteLine("Bir sayı giriniz :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz sayı : " + a);
+         }
+
+         catch(Exception ex)
+         {
+           Console.WriteLine(ex.Message);
+         }
+         finally
+         {
+             Console.WriteLine("İşlem Tamalandı");
+         }
+              Console.WriteLine("------------------");
+            try
+            {
+                int c = int.Parse(null);
+                
+            }
+            catch (ArgumentNullException ex)
+            {
+              Console.WriteLine("Boş değer girdiniz");
+              Console.WriteLine(ex);    
+            
+            }
+
+            Console.WriteLine("------------------");
+
+             try
+            {
+                
+                int d = int.Parse("test");
+            }
+              catch (FormatException ex )
+            {
+              Console.WriteLine("Veri tipi uygun değil");
+              Console.WriteLine(ex);    
+            
+            }
+
+              Console.WriteLine("------------------");
+
+             try
+            {
+                
+                int d = int.Parse("-55000000000");
+            }
+              catch (OverflowException ex )
+            {
+              Console.WriteLine("Çok küçük veya büyük değer girdiniz.");
+              Console.WriteLine(ex);    
+            
+            }
+           
+
+
 
        }
 
