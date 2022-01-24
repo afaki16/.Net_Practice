2e458cb7357fdc49757d4a7529313aa934259085
afaki16 <alperenfaki@gmail.com>
2022-01-24 21:55:24 +0300

    new

--- If_Else/Program.cs
+++ If_Else/Program.cs
@@ -8,8 +8,27 @@ namespace If_Else
 
         static void Main(string[] args)
         {
-           
-            
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time < 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+
+            string a = time >= 6 && time <= 11 ? "Günaydın!" : time <= 18 ? "İyi günler!" : "İyi geceler!";  
+
+
+
+
         }
 
     }
