130f47b9b22752b2bd3c424c046fc602111d74d9
afaki16 <alperenfaki@gmail.com>
2022-01-24 20:57:16 +0300

    new

---
+++ Try_Catch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    
+    class Program
+    {
+       
+       static void Main(string[] args)
+       {
+         Console.WriteLine("Bir sayı giriniz :");
+
+       }
+
+
+
+    }
+
+
+
+
+
+}
