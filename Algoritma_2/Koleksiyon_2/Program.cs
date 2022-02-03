d49f5dbe42de329888eef9bba80d3ee68dfd2033
afaki16 <alperenfaki@gmail.com>
2022-02-03 13:32:01 +0300

    New

---
+++ Algoritma_2/Koleksiyon_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Koleksiyon_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
