3a4e8a754fde09bbb2c0c67abbad5b0e4fcddff0
afaki16 <alperenfaki@gmail.com>
2022-02-03 21:30:55 +0300

    New

---
+++ Siniflar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Siniflar // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisanlar calisan = new Calisanlar();
+            calisan.Ad = "Alperen";
+            calisan.Soyad = "FAKI";
+            calisan.No = 11;
+            calisan.Departman = "IT";
+            calisan.CalisanBilgileri();
+        }
+    }
+
+    class Calisanlar
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+      
+       public void CalisanBilgileri()
+       {
+         Console.WriteLine("Çalışan Adı : {0}",Ad);
+         Console.WriteLine("Çalışan Soyadı : {0}",Soyad);
+         Console.WriteLine("Çalışan Numarası : {0}",No);
+         Console.WriteLine("Çalışan Departmanı : {0}",Departman);
+
+       }
+
+
+
+    }
+}
