a3398af249a874261e0cab155136b7ff7e824f6f
afaki16 <alperenfaki@gmail.com>
2022-02-03 16:36:13 +0300

    New

---
+++ Algoritma_2/Koleksiyon_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyon_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz : ");
+            string n = Console.ReadLine();
+            ArrayList sesli = new ArrayList();
+            char[] sabit_sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+
+            char[] harfler = new char[n.Length];
+            harfler =n.ToCharArray();
+
+            foreach (char giris in harfler)
+            {
+                foreach (char sabit in sabit_sesliler)
+                {
+                    if(giris==sabit)
+                    sesli.Add(giris);
+                }
+            }
+            foreach (var item in sesli)
+            {
+                Console.Write(item + " ");
+            }
+            
+             	
+       
+        }
+    }
+}
