41f75d35a8c75ea1769c252069156d2303178976
afaki16 <alperenfaki@gmail.com>
2022-02-05 14:12:40 +0300

    New

---
+++ Enum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Salı);
+
+            Console.WriteLine("Hava Sıcaklığını Giriniz:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n<=(int)HavaDurumu.Normal)
+            {
+            Console.WriteLine("Hava normalden daha kötü durumda...");
+            }
+            else if(n >= (int)HavaDurumu.Sicak)
+            {
+            Console.WriteLine("Dışarısı sıcak...");
+            }
+            else if(n >= (int)HavaDurumu.CokSicak)
+            {
+            Console.WriteLine("Evde otur..");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+      Pazartesi=1,
+      Salı,
+      Çarşamba,
+      Perşembe,
+      Cuma,
+      Cumartesi,
+      Pazar
+
+
+    }
+    enum HavaDurumu
+    {
+        Soğuk=5,
+        Normal=20,
+        Sicak=25,
+        CokSicak=35
+    }
+}
