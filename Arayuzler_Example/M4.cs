ebaf1d922a69ed4b18a3d23af72232d8e4188ede
afaki16 <alperenfaki@gmail.com>
2022-02-07 17:35:59 +0300

    New

---
+++ Arayuzler_Example/M4.cs
@@ -0,0 +1,20 @@
+namespace Arayuzler_Example
+{
+  public class M4:IOtomobil
+  {
+      public markalar marka()
+      {
+        return markalar.BMW;
+      }
+      public renkler renk()
+      {
+          return renkler.Gri;
+      }
+      public int tekerlekSayisi()
+      {
+          return 4;
+      }
+
+
+  }
+}
---
+++ Arayuzler_Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arayuzler_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Focus focus =  new Focus();
+            Console.WriteLine(focus.marka().ToString());
+            Console.WriteLine(focus.renk().ToString());
+            Console.WriteLine(focus.tekerlekSayisi());
+
+            Civic civic =  new();
+            Console.WriteLine(civic.marka().ToString());
+            Console.WriteLine(civic.renk().ToString());
+            Console.WriteLine(civic.tekerlekSayisi());
+
+        }
+    }
+}
